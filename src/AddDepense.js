80299128450cedab5b7b869e69d9eae5f83445a3
Olivier Lemaitre <olivierlemaitre@MacBook-Pro-de-Olivier.local>
2016-08-10 22:10:16 -0400

    Création du component input

--- src/AddDepense.js
+++ src/AddDepense.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import Input from './Input';
 
 class AddDepense extends Component {
+	constructor(props) {
+	    super(props);
 
-  testOli = () => {
-    this.props.onBtnSave(5);
-  }
+	    this.state = {
+	    	categorie:'Voiture',
+	      	nomDep: '',
+	      	depense: 0,
+	      	desc: ''
+	    };
+	}
 
-  render() {
-    return (
-	    <div className="col-md-4 col-sm-5 formAjout">
-	    	<h3>Ajouter</h3>
-	    	<select name="" className="form-control">
-				<option value="">Choisir un catégorie...</option>
-				{this.props.listeCat.map(({nom,idCat}) =>
-			    	<option key={idCat}>{nom}</option>		
-				)}
-	    	</select>
-	    	<input type="text" className="form-control" placeholder="Nom de la dépense"/>
-	    	<input type="text" className="form-control" placeholder="montant"/>
-	    	<textarea className="form-control" placeholder="Note sur la dépense"></textarea>
-	    	<button className="btn btn-primary form-control" onClick={this.testOli.bind()}>Ajouter</button>
-	    </div>
-    );
-  }
+	handleClickForm = () => {
+		this.props.onBtnSave(5);
+	}
+
+	handleChangeState = (id,value) => {
+	    this.setState({[id]:value});
+	    console.log(this.state.nomDep + '--' +this.state.montant);
+	}
+
+	render() {
+		return (
+		    <div className="col-md-4 col-sm-5 formAjout">
+		    	<h3>Ajouter</h3>
+		    	<select name="" className="form-control">
+					<option value="">Choisir un catégorie...</option>
+					{this.props.listeCat.map(({nom,idCat}) =>
+				    	<option key={idCat}>{nom}</option>		
+					)}
+		    	</select>
+		    	<Input id="nomDep" value={this.state.nomDep} onChange={this.handleChangeState} placeholder="Nom de la dépense"/>
+		    	<Input id="montant" value={this.state.montant} onChange={this.handleChangeState} placeholder="Montant"/>
+		    	<textarea className="form-control" placeholder="Note sur la dépense"></textarea>
+		    	<button className="btn btn-primary form-control" onClick={this.handleClickForm.bind()}>Ajouter</button>
+		    </div>
+		);
+	}
 }
 
 export default AddDepense; 
---
+++ src/Input.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+
+	handleChange = (event) => {
+		console.log(event.target.value);
+		this.props.onChange(event.target.id,event.target.value);
+	}
+
+	render() {
+		return (
+		    <input type="text" id={this.props.id} className="form-control" onChange={this.handleChange} value={this.props.value} placeholder={this.props.placeholder} />
+		);
+	}
+}
+
+export default Input; 
